96f1b2ac87f74e45bc7fc02b37df85088ece61b3
ArthurSens <arthursens2005@gmail.com>
2022-07-14 18:56:08 +0000

    Implement prober
    Signed-off-by: ArthurSens <arthursens2005@gmail.com>

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -17,21 +19,51 @@ const (
 	namespace     = "http_prober"
 	metricsPath   = "/metrics"
 	listenAddress = ":8080"
+	probeURL      = "https://www.google.com/"
+	interval      = 60
+	timeout       = 60
 )
 
 var (
-	buckets = []float64{1, 2, 4, 8, 16, 32, 64}
-	logger  log.Logger
+	logger           log.Logger
+	requestsDuration = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Name:      "probe_duration_seconds",
+		Help:      "Duration of HTTP probes.",
+		Buckets:   []float64{1, 2, 4, 8, 16, 32},
+	})
+	// One for total is not needed because a histogram already has a counter
+	requestsFailed = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "probes_failed_total",
+		Help:      "Amount of probes performed.",
+	})
 )
 
 func init() {
 	prometheus.MustRegister(version.NewCollector(namespace))
+	prometheus.MustRegister(requestsFailed)
+	prometheus.MustRegister(requestsDuration)
 }
 
 func main() {
 	logger = promlog.New(&promlog.Config{})
 	level.Info(logger).Log("msg", "Starting http-prober", "version", version.Info())
 	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
+
+	go func() {
+		t := time.NewTicker(interval * time.Second)
+		defer t.Stop()
+		for {
+			select {
+			case <-t.C:
+				err := Probe()
+				if err != nil {
+					level.Info(logger).Log("msg", "Failed probe", "err", err)
+				}
+			}
+		}
+	}()
 
 	http.Handle(metricsPath, promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -51,4 +83,29 @@ func main() {
 		level.Error(logger).Log("err", err)
 		os.Exit(1)
 	}
+}
+
+func Probe() error {
+	req, err := http.NewRequest("GET", probeURL, nil)
+	if err != nil {
+		return fmt.Errorf("Failed building request for url: %s", probeURL)
+	}
+	client := http.Client{
+		Timeout: timeout * time.Second,
+	}
+
+	start := time.Now()
+	res, err := client.Do(req)
+	duration := float64(time.Since(start)) / float64(time.Second)
+	if err != nil {
+		requestsFailed.Inc()
+		requestsDuration.Observe(duration)
+		return fmt.Errorf("Failed request for url: %s", probeURL)
+	}
+	defer res.Body.Close()
+
+	level.Info(logger).Log("msg", "Probed", "url", probeURL, "code", res.StatusCode)
+	requestsDuration.Observe(duration)
+
+	return nil
 }
